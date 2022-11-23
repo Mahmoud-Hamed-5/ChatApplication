046df2e0a1e683173018bb56ed5e31fcab83800e
Mahmoud Hamed <mahmoud.hamed91.mh@gmail.com>
2022-11-23 22:37:30 +0200

    Custom Registration and Login forms

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\SampleController;
+use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,11 +19,17 @@ Route::get('/', function () {
 });
 
 
-Route::controller(SampleController::class)->group(function(){
+Route::controller(AuthController::class)->group(function(){
 
     Route::get('login', 'index')->name('login');
 
     Route::get('register', 'register')->name('register');
 
     Route::get('logout', 'logout')->name('logout');
+
+    Route::post('validate_registration', 'validate_registration')->name('sample.validate_registration');
+
+    Route::post('validate_login', 'validate_login')->name('sample.validate_login');
+
+    Route::get('dashboard', 'dashboard')->name('dashboard');
 });
